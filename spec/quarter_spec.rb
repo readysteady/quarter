62c7d21fbff445f6b58cd11ee33f02575ce10e64
Tim Craft <mail@timcraft.com>
2020-11-03 11:02:24 +0000

    Convert spec to rspec syntax

--- spec/quarter_spec.rb
+++ spec/quarter_spec.rb
@@ -1,98 +1,110 @@
-require 'minitest/autorun'
-require 'minitest/global_expectations'
 require_relative '../lib/quarter'
 require_relative '../lib/quarter/yaml'
 require 'month'
 
-describe 'Quarter class' do
+RSpec.describe Quarter do
   let(:year) { 2020 }
   let(:number) { 1 }
   let(:quarter) { Quarter.new(year, number) }
 
   describe '#initialize' do
-    it 'raises an exception given an invalid number' do
-      proc { Quarter.new(year, 0) }.must_raise(ArgumentError)
-      proc { Quarter.new(year, 5) }.must_raise(ArgumentError)
+    it 'returns frozen instances' do
+      expect(quarter.frozen?).to eq(true)
     end
 
-    it 'returns frozen instances' do
-      quarter.frozen?.must_equal(true)
+    context 'with an invalid number' do
+      it 'raises an exception' do
+        expect { Quarter.new(year, 0) }.to raise_error(ArgumentError)
+        expect { Quarter.new(year, 5) }.to raise_error(ArgumentError)
+      end
     end
   end
 
   describe '#year' do
     it 'returns the year' do
-      quarter.year.must_equal(year)
+      expect(quarter.year).to eq(year)
     end
   end
 
   describe '#number' do
     it 'returns the number' do
-      quarter.number.must_equal(number)
+      expect(quarter.number).to eq(number)
     end
   end
 
   describe '#name' do
     it 'returns a string containing the number' do
-      quarter.name.must_equal('Q1')
+      expect(quarter.name).to eq('Q1')
     end
   end
 
   describe '#to_s' do
     it 'returns a string containing the year and the number' do
-      quarter.to_s.must_equal('Q1 2020')
+      expect(quarter.to_s).to eq('Q1 2020')
     end
   end
 
   describe '#iso8601' do
     it 'returns a string containing the year and the number' do
-      quarter.iso8601.must_equal('2020-Q1')
+      expect(quarter.iso8601).to eq('2020-Q1')
     end
   end
 
   describe '#hash' do
     it 'returns the same value for instances with the same year and number' do
-      Quarter.new(year, number).hash.must_equal(Quarter.new(year, number).hash)
+      expect(Quarter.new(year, number).hash).to eq(Quarter.new(year, number).hash)
     end
   end
 
   describe '#<=>' do
-    it 'returns -1 if the argument comes after' do
-      value = Quarter.new(year, 1) <=> Quarter.new(year, 2)
-      value.must_equal(-1)
+    context 'when the argument comes after' do
+      it 'returns -1' do
+        value = Quarter.new(year, 1) <=> Quarter.new(year, 2)
+
+        expect(value).to eq(-1)
+      end
     end
 
-    it 'returns 0 if the argument is the same year and number' do
-      value = Quarter.new(year, number) <=> Quarter.new(year, number)
-      value.must_equal(0)
+    context 'when the argument is the same year and number' do
+      it 'returns 0' do
+        value = Quarter.new(year, number) <=> Quarter.new(year, number)
+
+        expect(value).to eq(0)
+      end
     end
 
-    it 'returns -1 if the argument comes before' do
-      value = Quarter.new(year, 2) <=> Quarter.new(year, 1)
-      value.must_equal(1)
+    context 'when the argument comes before' do
+      it 'returns -1' do
+        value = Quarter.new(year, 2) <=> Quarter.new(year, 1)
+
+        expect(value).to eq(1)
+      end
     end
 
-    it 'returns nil if the argument is nil' do
-      value = Quarter.new(year, 1) <=> nil
-      value.must_be_nil
+    context 'when the argument is nil' do
+      it 'returns nil' do
+        value = Quarter.new(year, 1) <=> nil
+
+        expect(value).to be_nil
+      end
     end
   end
 
   describe '#next' do
     it 'returns the next quarter' do
-      Quarter.new(year, 1).next.must_equal(Quarter.new(year, 2))
-      Quarter.new(year, 2).next.must_equal(Quarter.new(year, 3))
-      Quarter.new(year, 3).next.must_equal(Quarter.new(year, 4))
-      Quarter.new(year, 4).next.must_equal(Quarter.new(year + 1, 1))
+      expect(Quarter.new(year, 1).next).to eq(Quarter.new(year, 2))
+      expect(Quarter.new(year, 2).next).to eq(Quarter.new(year, 3))
+      expect(Quarter.new(year, 3).next).to eq(Quarter.new(year, 4))
+      expect(Quarter.new(year, 4).next).to eq(Quarter.new(year + 1, 1))
     end
   end
 
   describe '#prev' do
     it 'returns the previous quarter' do
-      Quarter.new(year, 1).prev.must_equal(Quarter.new(year - 1, 4))
-      Quarter.new(year, 2).prev.must_equal(Quarter.new(year, 1))
-      Quarter.new(year, 3).prev.must_equal(Quarter.new(year, 2))
-      Quarter.new(year, 4).prev.must_equal(Quarter.new(year, 3))
+      expect(Quarter.new(year, 1).prev).to eq(Quarter.new(year - 1, 4))
+      expect(Quarter.new(year, 2).prev).to eq(Quarter.new(year, 1))
+      expect(Quarter.new(year, 3).prev).to eq(Quarter.new(year, 2))
+      expect(Quarter.new(year, 4).prev).to eq(Quarter.new(year, 3))
     end
   end
 
@@ -100,14 +112,14 @@ describe 'Quarter class' do
     it 'enumerates quarters up to the given limit' do
       enumerator = Quarter.new(year, 1).upto(Quarter.new(year, 4))
 
-      enumerator.must_be_instance_of(Enumerator)
+      expect(enumerator).to be_an(Enumerator)
 
-      enumerator.next.must_equal(Quarter.new(year, 1))
-      enumerator.next.must_equal(Quarter.new(year, 2))
-      enumerator.next.must_equal(Quarter.new(year, 3))
-      enumerator.next.must_equal(Quarter.new(year, 4))
+      expect(enumerator.next).to eq(Quarter.new(year, 1))
+      expect(enumerator.next).to eq(Quarter.new(year, 2))
+      expect(enumerator.next).to eq(Quarter.new(year, 3))
+      expect(enumerator.next).to eq(Quarter.new(year, 4))
 
-      proc { enumerator.next }.must_raise(StopIteration)
+      expect { enumerator.next }.to raise_error(StopIteration)
     end
   end
 
@@ -115,233 +127,239 @@ describe 'Quarter class' do
     it 'enumerates quarters down to the given limit' do
       enumerator = Quarter.new(year, 4).downto(Quarter.new(year, 1))
 
-      enumerator.must_be_instance_of(Enumerator)
+      expect(enumerator).to be_an(Enumerator)
 
-      enumerator.next.must_equal(Quarter.new(year, 4))
-      enumerator.next.must_equal(Quarter.new(year, 3))
-      enumerator.next.must_equal(Quarter.new(year, 2))
-      enumerator.next.must_equal(Quarter.new(year, 1))
+      expect(enumerator.next).to eq(Quarter.new(year, 4))
+      expect(enumerator.next).to eq(Quarter.new(year, 3))
+      expect(enumerator.next).to eq(Quarter.new(year, 2))
+      expect(enumerator.next).to eq(Quarter.new(year, 1))
 
-      proc { enumerator.next }.must_raise(StopIteration)
+      expect { enumerator.next }.to raise_error(StopIteration)
     end
   end
 
   describe '#+' do
     it 'returns the quarter the given number of quarters after' do
-      (Quarter.new(year, 1) + 1).must_equal(Quarter.new(year, 2))
-      (Quarter.new(year, 2) + 1).must_equal(Quarter.new(year, 3))
-      (Quarter.new(year, 3) + 1).must_equal(Quarter.new(year, 4))
-      (Quarter.new(year, 4) + 1).must_equal(Quarter.new(year + 1, 1))
+      expect(Quarter.new(year, 1) + 1).to eq(Quarter.new(year, 2))
+      expect(Quarter.new(year, 2) + 1).to eq(Quarter.new(year, 3))
+      expect(Quarter.new(year, 3) + 1).to eq(Quarter.new(year, 4))
+      expect(Quarter.new(year, 4) + 1).to eq(Quarter.new(year + 1, 1))
 
-      (Quarter.new(year, 1) + 2).must_equal(Quarter.new(year, 3))
-      (Quarter.new(year, 2) + 2).must_equal(Quarter.new(year, 4))
-      (Quarter.new(year, 3) + 2).must_equal(Quarter.new(year + 1, 1))
-      (Quarter.new(year, 4) + 2).must_equal(Quarter.new(year + 1, 2))
+      expect(Quarter.new(year, 1) + 2).to eq(Quarter.new(year, 3))
+      expect(Quarter.new(year, 2) + 2).to eq(Quarter.new(year, 4))
+      expect(Quarter.new(year, 3) + 2).to eq(Quarter.new(year + 1, 1))
+      expect(Quarter.new(year, 4) + 2).to eq(Quarter.new(year + 1, 2))
     end
   end
 
   describe '#-' do
     it 'returns the quarter the given number of quarters before' do
-      (Quarter.new(year, 1) - 1).must_equal(Quarter.new(year - 1, 4))
-      (Quarter.new(year, 2) - 1).must_equal(Quarter.new(year, 1))
-      (Quarter.new(year, 3) - 1).must_equal(Quarter.new(year, 2))
-      (Quarter.new(year, 4) - 1).must_equal(Quarter.new(year, 3))
+      expect(Quarter.new(year, 1) - 1).to eq(Quarter.new(year - 1, 4))
+      expect(Quarter.new(year, 2) - 1).to eq(Quarter.new(year, 1))
+      expect(Quarter.new(year, 3) - 1).to eq(Quarter.new(year, 2))
+      expect(Quarter.new(year, 4) - 1).to eq(Quarter.new(year, 3))
 
-      (Quarter.new(year, 1) - 2).must_equal(Quarter.new(year - 1, 3))
-      (Quarter.new(year, 2) - 2).must_equal(Quarter.new(year - 1, 4))
-      (Quarter.new(year, 3) - 2).must_equal(Quarter.new(year, 1))
-      (Quarter.new(year, 4) - 2).must_equal(Quarter.new(year, 2))
+      expect(Quarter.new(year, 1) - 2).to eq(Quarter.new(year - 1, 3))
+      expect(Quarter.new(year, 2) - 2).to eq(Quarter.new(year - 1, 4))
+      expect(Quarter.new(year, 3) - 2).to eq(Quarter.new(year, 1))
+      expect(Quarter.new(year, 4) - 2).to eq(Quarter.new(year, 2))
     end
 
     it 'returns the number of quarters between the two quarters' do
-      (Quarter.new(year, 1) - Quarter.new(year, 1)).must_equal(0)
-      (Quarter.new(year, 4) - Quarter.new(year, 1)).must_equal(3)
-      (Quarter.new(year + 3, 1) - Quarter.new(year, 1)).must_equal(12)
+      expect(Quarter.new(year, 1) - Quarter.new(year, 1)).to eq(0)
+      expect(Quarter.new(year, 4) - Quarter.new(year, 1)).to eq(3)
+      expect(Quarter.new(year + 3, 1) - Quarter.new(year, 1)).to eq(12)
     end
   end
 
   describe '#include?' do
     it 'returns true when the quarter includes the given date' do
-      quarter.include?(Date.new(year, 1, 1)).must_equal(true)
+      expect(quarter.include?(Date.new(year, 1, 1))).to eq(true)
     end
 
     it 'returns false otherwise' do
-      quarter.include?(Date.new(year, 4, 1)).must_equal(false)
+      expect(quarter.include?(Date.new(year, 4, 1))).to eq(false)
     end
   end
 
   describe '#===' do
     it 'returns true when the quarter includes the given date' do
-      (quarter === Date.new(year, 1, 1)).must_equal(true)
+      expect(quarter === Date.new(year, 1, 1)).to eq(true)
     end
 
     it 'returns false otherwise' do
-      (quarter === Date.new(year, 4, 1)).must_equal(false)
+      expect(quarter === Date.new(year, 4, 1)).to eq(false)
     end
   end
 
   describe '#start_date' do
     it 'returns the first date in the quarter' do
-      Quarter.new(year, 1).start_date.must_equal(Date.new(year, 1, 1))
-      Quarter.new(year, 2).start_date.must_equal(Date.new(year, 4, 1))
-      Quarter.new(year, 3).start_date.must_equal(Date.new(year, 7, 1))
-      Quarter.new(year, 4).start_date.must_equal(Date.new(year, 10, 1))
+      expect(Quarter.new(year, 1).start_date).to eq(Date.new(year, 1, 1))
+      expect(Quarter.new(year, 2).start_date).to eq(Date.new(year, 4, 1))
+      expect(Quarter.new(year, 3).start_date).to eq(Date.new(year, 7, 1))
+      expect(Quarter.new(year, 4).start_date).to eq(Date.new(year, 10, 1))
     end
   end
 
   describe '#end_date' do
     it 'returns the last date in the quarter' do
-      Quarter.new(year, 1).end_date.must_equal(Date.new(year, 3, 31))
-      Quarter.new(year, 2).end_date.must_equal(Date.new(year, 6, 30))
-      Quarter.new(year, 3).end_date.must_equal(Date.new(year, 9, 30))
-      Quarter.new(year, 4).end_date.must_equal(Date.new(year, 12, 31))
+      expect(Quarter.new(year, 1).end_date).to eq(Date.new(year, 3, 31))
+      expect(Quarter.new(year, 2).end_date).to eq(Date.new(year, 6, 30))
+      expect(Quarter.new(year, 3).end_date).to eq(Date.new(year, 9, 30))
+      expect(Quarter.new(year, 4).end_date).to eq(Date.new(year, 12, 31))
     end
   end
 
   describe '#dates' do
     it 'returns the range of dates in the quarter' do
       range = Quarter.new(year, 2).dates
-      range.must_be_instance_of(Range)
-      range.count.must_equal(91)
-      range.all? { |date| Date === date }.must_equal(true)
-      range.first.must_equal(Date.new(year, 4, 1))
-      range.last.must_equal(Date.new(year, 6, 30))
+
+      expect(range).to be_a(Range)
+      expect(range.count).to eq(91)
+      expect(range.all? { |date| Date === date }).to eq(true)
+      expect(range.first).to eq(Date.new(year, 4, 1))
+      expect(range.last).to eq(Date.new(year, 6, 30))
     end
   end
 
   describe '#length' do
     it 'returns the integer number of days in the quarter' do
-      Quarter.new(2000, 1).length.must_equal(91)
-      Quarter.new(2001, 1).length.must_equal(90)
-      Quarter.new(year, 2).length.must_equal(91)
-      Quarter.new(year, 3).length.must_equal(92)
-      Quarter.new(year, 4).length.must_equal(92)
+      expect(Quarter.new(2000, 1).length).to eq(91)
+      expect(Quarter.new(2001, 1).length).to eq(90)
+      expect(Quarter.new(year, 2).length).to eq(91)
+      expect(Quarter.new(year, 3).length).to eq(92)
+      expect(Quarter.new(year, 4).length).to eq(92)
     end
   end
 
   describe '#months' do
     it 'returns the range of months in the quarter' do
       range = quarter.months
-      range.must_be_instance_of(Range)
-      range.count.must_equal(3)
-      range.all? { |month| Month === month }.must_equal(true)
-      range.first.must_equal(Month.new(year, 1))
-      range.last.must_equal(Month.new(year, 3))
+
+      expect(range).to be_a(Range)
+      expect(range.count).to eq(3)
+      expect(range.all? { |month| Month === month }).to eq(true)
+      expect(range.first).to eq(Month.new(year, 1))
+      expect(range.last).to eq(Month.new(year, 3))
     end
   end
 end
 
-describe 'Quarter method' do
+RSpec.describe 'Quarter(object)' do
   let(:year) { 2020 }
   let(:number) { 1 }
   let(:quarter) { Quarter.new(year, number) }
 
   it 'returns the quarter for the given date' do
-    Quarter(Date.new(year, 1, 1)).must_equal(quarter)
+    expect(Quarter(Date.new(year, 1, 1))).to eq(quarter)
   end
 
   it 'returns the quarter for the given time' do
-    Quarter(Time.utc(year, 1, 1)).must_equal(quarter)
+    expect(Quarter(Time.utc(year, 1, 1))).to eq(quarter)
   end
 
   it 'returns the quarter for the given datetime' do
-    Quarter(DateTime.parse('2001-02-03T04:05:06+07:00')).must_equal(Quarter.new(2001, 1))
+    expect(Quarter(DateTime.parse('2001-02-03T04:05:06+07:00'))).to eq(Quarter.new(2001, 1))
   end
 
   it 'returns the given quarter' do
-    Quarter(quarter).object_id.must_equal(quarter.object_id)
+    expect(Quarter(quarter).object_id).to eq(quarter.object_id)
   end
 
   it 'returns the quarter number for the given month number' do
-    Quarter(1).must_equal(1)
-    Quarter(4).must_equal(2)
-    Quarter(7).must_equal(3)
-    Quarter(10).must_equal(4)
+    expect(Quarter(1)).to eq(1)
+    expect(Quarter(4)).to eq(2)
+    expect(Quarter(7)).to eq(3)
+    expect(Quarter(10)).to eq(4)
   end
 end
 
-describe 'Quarter.parse' do
+RSpec.describe 'Quarter.parse' do
   it 'returns the quarter for the given string representation' do
-    Quarter.parse('2020-Q1').must_equal(Quarter.new(2020, 1))
-    Quarter.parse('Q1 2020').must_equal(Quarter.new(2020, 1))
+    expect(Quarter.parse('2020-Q1')).to eq(Quarter.new(2020, 1))
+    expect(Quarter.parse('Q1 2020')).to eq(Quarter.new(2020, 1))
   end
 
   it 'raises an exception given an invalid string representation' do
-    proc { Quarter.parse('Q1') }.must_raise(ArgumentError)
-    proc { Quarter.parse('2020') }.must_raise(ArgumentError)
-    proc { Quarter.parse('quarter') }.must_raise(ArgumentError)
+    expect { Quarter.parse('Q1') }.to raise_error(ArgumentError)
+    expect { Quarter.parse('2020') }.to raise_error(ArgumentError)
+    expect { Quarter.parse('quarter') }.to raise_error(ArgumentError)
   end
 end
 
-describe 'Quarter.today' do
+RSpec.describe 'Quarter.today' do
   let(:current_quarter) { Quarter(Date.today) }
 
   it 'returns the current quarter' do
-    Quarter.today.must_equal(current_quarter)
+    expect(Quarter.today).to eq(current_quarter)
   end
 end
 
-describe 'Quarter.now' do
+RSpec.describe 'Quarter.now' do
   let(:current_quarter) { Quarter(Time.now) }
 
   it 'returns the current quarter' do
-    Quarter.now.must_equal(current_quarter)
+    expect(Quarter.now).to eq(current_quarter)
   end
 end
 
-describe 'YAML' do
+RSpec.describe 'YAML' do
   let(:quarter) { Quarter.new(2020, 1) }
 
-  it 'dumps quarter objects' do
-    YAML.dump([quarter]).must_equal("---\n- Q1 2020\n")
+  describe '.dump' do
+    it 'dumps quarter objects' do
+      expect(YAML.dump([quarter])).to eq("---\n- Q1 2020\n")
+    end
   end
 
-  it 'loads quarter objects' do
-    YAML.load("---\n- Q1 2020\n").must_equal([quarter])
+  describe '.load' do
+    it 'loads quarter objects' do
+      expect(YAML.load("---\n- Q1 2020\n")).to eq([quarter])
 
-    YAML.load("---\n- 2020-Q1\n").must_equal([quarter])
-    YAML.load("---\n- 2020/Q1\n").must_equal([quarter])
+      expect(YAML.load("---\n- 2020-Q1\n")).to eq([quarter])
+      expect(YAML.load("---\n- 2020/Q1\n")).to eq([quarter])
 
-    YAML.load("---\n- Q12020\n").must_equal([quarter])
-    YAML.load("---\n- 2020Q1\n").must_equal([quarter])
+      expect(YAML.load("---\n- Q12020\n")).to eq([quarter])
+      expect(YAML.load("---\n- 2020Q1\n")).to eq([quarter])
+    end
   end
 end
 
-describe 'Quarter::Methods' do
+RSpec.describe Quarter::Methods do
   include Quarter::Methods
 
   describe 'Q1' do
     it 'returns the 1st quarter' do
-      Q1(2020).must_equal(Quarter.new(2020, 1))
+      expect(Q1(2020)).to eq(Quarter.new(2020, 1))
     end
   end
 
   describe 'Q2' do
     it 'returns the 2nd quarter' do
-      Q2(2020).must_equal(Quarter.new(2020, 2))
+      expect(Q2(2020)).to eq(Quarter.new(2020, 2))
     end
   end
 
   describe 'Q3' do
     it 'returns the 3rd quarter' do
-      Q3(2020).must_equal(Quarter.new(2020, 3))
+      expect(Q3(2020)).to eq(Quarter.new(2020, 3))
     end
   end
 
   describe 'Q4' do
     it 'returns the 4th quarter' do
-      Q4(2020).must_equal(Quarter.new(2020, 4))
+      expect(Q4(2020)).to eq(Quarter.new(2020, 4))
     end
   end
 end
 
-describe 'Quarter::Constants' do
+RSpec.describe Quarter::Constants do
   describe 'Q1' do
     it 'returns the 1st quarter' do
       Q1 = Quarter::Constants::Q1
 
-      (2020-Q1).must_equal(Quarter.new(2020, 1))
-      (2020/Q1).must_equal(Quarter.new(2020, 1))
+      expect(2020-Q1).to eq(Quarter.new(2020, 1))
+      expect(2020/Q1).to eq(Quarter.new(2020, 1))
     end
   end
 
@@ -349,8 +367,8 @@ describe 'Quarter::Constants' do
     it 'returns the 2nd quarter' do
       Q2 = Quarter::Constants::Q2
 
-      (2020-Q2).must_equal(Quarter.new(2020, 2))
-      (2020/Q2).must_equal(Quarter.new(2020, 2))
+      expect(2020-Q2).to eq(Quarter.new(2020, 2))
+      expect(2020/Q2).to eq(Quarter.new(2020, 2))
     end
   end
 
@@ -358,8 +376,8 @@ describe 'Quarter::Constants' do
     it 'returns the 3rd quarter' do
       Q3 = Quarter::Constants::Q3
 
-      (2020-Q3).must_equal(Quarter.new(2020, 3))
-      (2020/Q3).must_equal(Quarter.new(2020, 3))
+      expect(2020-Q3).to eq(Quarter.new(2020, 3))
+      expect(2020/Q3).to eq(Quarter.new(2020, 3))
     end
   end
 
@@ -367,8 +385,8 @@ describe 'Quarter::Constants' do
     it 'returns the 4th quarter' do
       Q4 = Quarter::Constants::Q4
 
-      (2020-Q4).must_equal(Quarter.new(2020, 4))
-      (2020/Q4).must_equal(Quarter.new(2020, 4))
+      expect(2020-Q4).to eq(Quarter.new(2020, 4))
+      expect(2020/Q4).to eq(Quarter.new(2020, 4))
     end
   end
 end
